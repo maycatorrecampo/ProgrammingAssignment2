0b58a4bf65184f18c3677afd9390f3ba7e09ada9
maycatorrecampo <danielle.torrecampo@gmail.com>
2015-06-21 17:05:04 +0800

    Added comments and revised variable names for readability

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,19 @@
 ## makeCacheMatrix
 ## Creates a "special" object of functions that can cache
 ## the inverse of a matrix
+## Arguments: x - an invertible matrix
+## Returns: a list of getter/setter functions for storing
+##      and retrieving the original and inverted matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-  matrix <- NULL
+  cachematrix <- NULL #cache
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setInverse <- function(invmtrx) matrix <<- invmtrx
-  getInverse <- function() matrix
+  setInverse <- function(invmtrx) cachematrix <<- invmtrx
+  getInverse <- function() cachematrix
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
@@ -20,16 +23,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve
 ## Computes the inverse of the matrix using the "special"
 ## object from makeCacheMatrix.
+## Arguments: x - the result from makeCacheMatrix
+## Returns: invmatrix - the inverse of the original matrix
 
 cacheSolve <- function(x, ...) {
-  matrix <- x$getInverse()
+  invmatrix <- x$getInverse()
   ## Checks if cache is not empty
-  if(!is.null(matrix)){ 
+  if(!is.null(invmatrix)){ 
     message("getting cached data")
-    return(matrix)
+    return(invmatrix) #returns the contents of the cache
   }
-  grid <- x$get()
-  matrix <- solve(grid, ...)
-  x$setInverse(matrix)
-  matrix
+  ##Cache is empty, solve for the inverse
+  grid <- x$get() #gets the original matrix
+  invmatrix <- solve(grid, ...) #matrix is inverted
+  x$setInverse(invmatrix) #the inverted matrix is stored in the cache
+  invmatrix
 }
